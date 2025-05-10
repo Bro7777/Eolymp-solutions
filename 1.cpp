c8b1e57d33089301e9991b58cacbe051cb539b19
Bro7777 <156592045+Bro7777@users.noreply.github.com>
2025-05-10 11:07:08 +0400

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+// Problem: Eolymp 1 - Simple Problem
+// Link: https://basecamp.eolymp.com/en/problems/1
+
+#include <iostream>
+using namespace std;
+int main() {
+  long long a;
+  cin>>a;
+  cout<<a/10<<" "<<a%10;
+  return 0;
+}
+
+
