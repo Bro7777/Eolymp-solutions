5cc054622570523aae39451b78985b824e8c4a1f
Bro7777 <156592045+Bro7777@users.noreply.github.com>
2025-05-10 18:19:57 +0400

    Create 00017.cpp

---
+++ 00017.cpp
@@ -0,0 +1,16 @@
+// Problem: Eolymp 17 - Gardener Painter
+// Link: https://basecamp.eolymp.com/en/problems/17
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    unsigned long long n,k;
+    cin>>n;
+    unsigned long long v1=n-1;
+    cout<<fixed<<setprecision(0)<<3*pow(2,v1);
+}
