0aefeb2773a42d10a0d48cb5adcb6b855c0ebe14
Bro7777 <156592045+Bro7777@users.noreply.github.com>
2025-06-14 19:16:49 +0400

    Create 00063.cpp

---
+++ 00063.cpp
@@ -0,0 +1,14 @@
+// Problem: Eolymp 63 - Anfisa and the flowers
+// Link: https://basecamp.eolymp.com/en/problems/63
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long m,n,s;
+    cin>>m>>n;
+    s=(m-1)*(n-1)+1;
+    cout<<s;
+}
